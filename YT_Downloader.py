daad8493b3a194b500e6da618bebe19466a04e7b
Devansh Soni <53042477+ds-ux@users.noreply.github.com>
2020-01-18 21:25:20 +0530

    Update YT_Downloader.py
    Fixed a few bugs.

--- YT_Downloader.py
+++ YT_Downloader.py
@@ -15,16 +15,16 @@ import urllib.error
 from io import BytesIO
 from pytube import YouTube
 from PIL import Image, ImageTk
-
+from threading import Thread
 
 root = tk.Tk()
-root.geometry("450x190")
+root.geometry("450x190+700+300")
 root.resizable(0, 0)
 root.title("YT Downloader")
 
 try:
     root.tk.call('wm', 'iconphoto', root._w, ImageTk.PhotoImage(file='ic.ico'))
-except:
+except FileNotFoundError:
     pass
 
 root.configure(bg="#141414")
@@ -41,7 +41,9 @@ global yt, ad, stream
 
 def an_err():
     root.geometry("450x190")
-    tk.Label(fms, text="An Error occurred.", padx=18, pady=3, fg="#990f02", bg="#210000").place(rely=1.0, x=0, anchor="sw")
+    er_lbl = tk.Label(fms, text="An Error occurred.", padx=18, pady=3, fg="#990f02", bg="#210000")
+    er_lbl.place(rely=1.0, x=0, anchor="sw")
+    er_lbl.after(5000, er_lbl.destroy)
 
 
 def get_things():
@@ -59,7 +61,9 @@ def dwnl_vid():
     vid_str = stream.filter(progressive=True)
     try:
         vid_str.first().download()
-        tk.Label(fms, text="Video Downloaded.", padx=15, pady=3, fg="green", bg="#001414").place(rely=1.0, anchor="sw")
+        vid_lbl = tk.Label(fms, text="Video Downloaded.", padx=15, pady=3, fg="green", bg="#001414")
+        vid_lbl.place(rely=1.0, anchor="sw")
+        vid_lbl.after(5000, vid_lbl.destroy)
     except:
         an_err()
 
@@ -68,7 +72,9 @@ def dwnl_aud():
     aud_str = stream.filter(only_audio=True)
     try:
         aud_str.first().download()
-        tk.Label(fms, text="Audio Downloaded.", padx=15, pady=3, fg="green", bg="#001414").place(rely=1.0, anchor="sw")
+        aud_lbl = tk.Label(fms, text="Audio Downloaded.", padx=15, pady=3, fg="green", bg="#001414")
+        aud_lbl.place(rely=1.0, anchor="sw")
+        aud_lbl.after(5000, aud_lbl.destroy)
     except:
         an_err()
 
@@ -87,14 +93,18 @@ def show_info():
         pass
     else:
         root.geometry("450x190")
-        tk.Label(fms, text="Please Enter a URL.", padx=15, pady=3, fg="#990f02", bg="#210000").place(rely=1.0, anchor="sw")
+        url_lbl = tk.Label(fms, text="Please Enter a URL.", padx=15, pady=3, fg="#990f02", bg="#210000")
+        url_lbl.place(rely=1.0, anchor="sw")
+        url_lbl.after(5000, url_lbl.destroy)
         return
 
     try:
         urllib.request.urlopen("https://www.google.com", timeout=10)
     except urllib.error.URLError:
         root.geometry("450x190")
-        tk.Label(fms, text="No Network Connection.", padx=0, pady=3, fg="#990f02", bg="#210000").place(rely=1.0, anchor="sw")
+        no_net_lbl = tk.Label(fms, text="No Network Connection.", padx=0, pady=3, fg="#990f02", bg="#210000")
+        no_net_lbl.place(rely=1.0, anchor="sw")
+        no_net_lbl.after(5000, no_net_lbl.destroy)
         return
 
     ch = get_things()
@@ -121,13 +131,13 @@ def show_info():
     views = yt.views
     rt = "%.2f" % yt.rating
 
-    tk.Label(frs, text=f"Title :  {title[:33]}\n{title[33:70]}\n{title[70:95]}", padx=15, pady=3, bg="#141414", underline=0).pack(side='top')
+    tk.Label(frs, text=f"Title :  {title[:33]}\n{title[33:70]}\n{title[70:95]}", padx=15, pady=3, fg="#ffffff", bg="#141414", underline=0).pack(side='top')
 
-    tk.Label(frs, text=f"Views :  {views}", padx=15, pady=3, bg="#141414", underline=0).pack(side='top')
+    tk.Label(frs, text=f"Views :  {views}", padx=15, pady=3, fg="#ffffff", bg="#141414", underline=0).pack(side='top')
 
-    tk.Label(frs, text=f"Rating :  {rt}", padx=15, pady=3, bg="#141414", underline=0).pack(side='top')
+    tk.Label(frs, text=f"Rating :  {rt}", padx=15, pady=3, fg="#ffffff", bg="#141414", underline=0).pack(side='top')
 
-    tk.Label(frs, text=f"Download :", pady=3, bg="#141414", underline=0).pack(side="left", padx=(15, 0))
+    tk.Label(frs, text=f"Download :", pady=3, fg="#ffffff", bg="#141414", underline=0).pack(side="left", padx=(15, 0))
 
     tk.Button(frs, text='Video', bg="#001010", fg="#347c2c", padx=20, pady=3, font=10, borderwidth=2, command=dwnl_vid).pack(side='left', padx=10, pady=8)
 
@@ -136,16 +146,16 @@ def show_info():
 
 fr1 = tk.Frame(root, padx=10, pady=25, bg="#141414")
 fr1.pack()
-tk.Label(fr1, text="Enter URL   :", pady=15, padx=10, font=20, bg="#141414").pack(side="left")
+tk.Label(fr1, text="Enter URL   :", pady=15, padx=10, font=20, fg="#ffffff", bg="#141414").pack(side="left")
 
 link = tk.StringVar()
 
-en = tk.Entry(fr1, font=10, width=32, textvariable=link, bg="#212121").pack(side="right", padx=10)
+en = tk.Entry(fr1, font=10, width=32, textvariable=link, fg="#ffffff", bg="#212121").pack(side="right", padx=10)
 
 fr2 = tk.Frame(root, pady=0, padx=0, bg="#141414")
 fr2.pack(pady=5)
 
-tk.Button(fr2, text='Details', bg="#001010", padx=15, pady=3, font=10, borderwidth=2, command=show_info).pack(padx=10)
+tk.Button(fr2, text='Details', fg="#ffffff", bg="#001010", padx=15, pady=3, font=10, borderwidth=2, command=show_info).pack(padx=10)
 
 root.mainloop()
 
